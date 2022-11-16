955c04292ea198b73f94b2dd9b84461a7fb788e0
abcdq12345 <abcdq12345@naver.com>
2022-11-16 14:52:53 +0900

    Add : AttractionPage frame

--- enjoy_trip_FE/src/store/index.js
+++ enjoy_trip_FE/src/store/index.js
@@ -1,17 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    sidos: [{ value: null, text: "시도선택" }],
+    guguns: [{ value: null, text: "구군선택" }],
+    categories: [{ value: null, text: "전체" }],
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
