58c3ad5c972fe630cf4fd87cfe17cc61875af1ae
jangyejoo <abcdq12345@naver.com>
2022-11-21 12:27:32 +0900

    Add : pagination!!

--- enjoy_trip_FE/src/components/tourboard/TourBoardList.vue
+++ enjoy_trip_FE/src/components/tourboard/TourBoardList.vue
@@ -7,6 +7,11 @@
         :tour="tour"
       ></tour-board-list-item>
     </div>
+    <pagination-bar
+      :pageSetting="pageDataSetting(this.tours.length, limit, block, this.page)"
+      @paging="pagingMethod"
+    ></pagination-bar>
+
     <tour-board-detail></tour-board-detail>
   </b-container>
   <b-container v-else class="bv-example-row mt-3">
@@ -21,6 +26,7 @@
 <script>
 import TourBoardListItem from "@/components/tourboard/TourBoardListItem";
 import TourBoardDetail from "@/components/tourboard/TourBoardDetail.vue";
+import PaginationBar from "@/components/common/Pagination.vue";
 
 import { mapState } from "vuex";
 
@@ -29,9 +35,52 @@ export default {
   components: {
     TourBoardListItem,
     TourBoardDetail,
+    PaginationBar,
+  },
+  data() {
+    return { listData: [], page: 1, limit: 20, block: 5 };
   },
   computed: {
     ...mapState(["tours"]),
+  },
+  watch: {
+    tours: {
+      handler() {
+        this.pagingMethod(1);
+      },
+    },
+  },
+  mounted() {
+    this.pagingMethod(this.page);
+  },
+  methods: {
+    pagingMethod(page) {
+      this.listData = this.tours.slice(
+        (page - 1) * this.limit,
+        page * this.limit
+      );
+      this.page = page;
+      this.pageDataSetting(this.total, this.limit, this.block, page);
+    },
+    pageDataSetting(total, limit, block, page) {
+      const totalPage = Math.ceil(total / limit);
+      let currentPage = page;
+      const first =
+        currentPage > 1 ? parseInt(currentPage, 10) - parseInt(1, 10) : null;
+      const end =
+        totalPage !== currentPage
+          ? parseInt(currentPage, 10) + parseInt(1, 10)
+          : null;
+      let startIndex = (Math.ceil(currentPage / block) - 1) * block + 1;
+      let endIndex =
+        startIndex + block > totalPage ? totalPage : startIndex + block - 1;
+      let list = [];
+      for (let index = startIndex; index <= endIndex; index++) {
+        list.push(index);
+      }
+      const totalItemLength = this.tours.length;
+      return { first, end, list, currentPage, totalItemLength, totalPage };
+    },
   },
 };
 </script>
--- enjoy_trip_FE/src/router/index.js
+++ enjoy_trip_FE/src/router/index.js
@@ -7,34 +7,34 @@ import SignIn from "@/views/SignIn.vue";
 import SignUp from "@/views/SignUp.vue";
 import AppAttraction from "@/views/AppAttraction.vue";
 import AppTourBoard from "@/views/AppTourBoard.vue";
-import store from "@/store";
+// import store from "@/store";
 
 Vue.use(VueRouter);
 
-const onlyAuthUser = async (to, from, next) => {
-  console.log("author");
-  const checkUserInfo = store.getters["userStore/checkUserInfo"];
-  const checkToken = store.getters["userStore/checkToken"];
-  let token = sessionStorage.getItem("access-token");
-  console.log("---------------------");
-  console.log("userInfo");
-  console.log(checkUserInfo);
-  console.log("token");
-  console.log(checkToken);
-  console.log("로그인 처리 전", checkUserInfo, token);
-  if (checkUserInfo != null && token) {
-    console.log("토큰 유효성 체크하러 가자!!!!");
-    await store.dispatch("userStore/getUserInfo", token);
-  }
-  if (!checkToken || checkUserInfo === null) {
-    alert("로그인이 필요한 페이지입니다..");
-    // next({ name: "login" });
-    router.push("/signin");
-  } else {
-    console.log("로그인 했다!!!!!!!!!!!!!.");
-    next();
-  }
-};
+// const onlyAuthUser = async (to, from, next) => {
+//   console.log("author");
+//   const checkUserInfo = store.getters["userStore/checkUserInfo"];
+//   const checkToken = store.getters["userStore/checkToken"];
+//   let token = sessionStorage.getItem("access-token");
+//   console.log("---------------------");
+//   console.log("userInfo");
+//   console.log(checkUserInfo);
+//   console.log("token");
+//   console.log(checkToken);
+//   console.log("로그인 처리 전", checkUserInfo, token);
+//   if (checkUserInfo != null && token) {
+//     console.log("토큰 유효성 체크하러 가자!!!!");
+//     await store.dispatch("userStore/getUserInfo", token);
+//   }
+//   if (!checkToken || checkUserInfo === null) {
+//     alert("로그인이 필요한 페이지입니다..");
+//     // next({ name: "login" });
+//     router.push("/signin");
+//   } else {
+//     console.log("로그인 했다!!!!!!!!!!!!!.");
+//     next();
+//   }
+// };
 
 const routes = [
   {
@@ -62,7 +62,7 @@ const routes = [
     name: "Attraction",
     component: AppAttraction,
     // 임시
-    beforeEnter: onlyAuthUser,
+    // beforeEnter: onlyAuthUser,
     redirect: "/attraction/list",
     children: [
       {
