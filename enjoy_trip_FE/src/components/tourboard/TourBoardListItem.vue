2b3e5345d7e6db50fd0596ed78ca46a68cd67952
jangyejoo <abcdq12345@naver.com>
2022-11-23 09:36:13 +0900

    add swiper

--- enjoy_trip_FE/src/components/tourboard/TourBoardListItem.vue
+++ enjoy_trip_FE/src/components/tourboard/TourBoardListItem.vue
@@ -1,39 +1,47 @@
 <template>
-  <b-col class="mb-3">
-    <article
-      class="card mb-2"
-      style="max-width: 25rem, max-height:23 rem"
-      @click="selectTour"
-    >
-      <div class="img-wrapper" style="overflow: hidden">
-        <img
-          :src="tour.thumbNail"
-          alt="Card Image"
-          class="card-img-top"
-          style="height: 250px"
-        />
-      </div>
-      <div class="card-body">
-        <strong class="card-title">{{ tour.title }}</strong>
-        <div class="card-text">
-          <p class="mb-0">{{ tour.userName }}</p>
-          <p class="mb-0">
-            {{ tour.registerDate | moment("YYYY-MM-DD LT") }}
-            <span class="mb-0 float-right">
-              <font-awesome-icon icon="fa-solid fa-eye" /> {{ tour.hit }}</span
-            >
-          </p>
+  <swiper-slide>
+    <b-col class="mb-3">
+      <article
+        class="card mb-2"
+        style="max-width: 25rem, max-height:23 rem"
+        @click="selectTour"
+      >
+        <div class="img-wrapper" style="overflow: hidden">
+          <img
+            :src="tour.thumbNail"
+            alt="Card Image"
+            class="card-img-top"
+            style="height: 250px"
+          />
         </div>
-      </div>
-    </article>
-  </b-col>
+        <div class="card-body">
+          <strong class="card-title">{{ tour.title }}</strong>
+          <div class="card-text">
+            <p class="mb-0">{{ tour.userName }}</p>
+            <p class="mb-0">
+              {{ tour.registerDate | moment("YYYY-MM-DD LT") }}
+              <span class="mb-0 float-right">
+                <font-awesome-icon icon="fa-solid fa-eye" />
+                {{ tour.hit }}</span
+              >
+            </p>
+          </div>
+        </div>
+      </article>
+    </b-col>
+  </swiper-slide>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import { SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 
 export default {
   name: "TourBoardListItem",
+  components: {
+    SwiperSlide,
+  },
   props: {
     tour: Object,
   },
